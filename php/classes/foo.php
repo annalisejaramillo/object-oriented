b8397d656933eb6c9936b7e7a16d31e049928ba9
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-07-16 17:28:58 -0600

    Wrote and documented all variables in class author

--- php/classes/foo.php
+++ php/classes/foo.php
@@ -1,13 +1,53 @@
 <?php
-namespace ajaramillo208/objected-oriented;
+namespace Ajaramillo208\ObjectOriented;
 
 require_once("autoload.php");
 
-use validateDate;
-use validateUuid;
 
 /**
  *
+ *this is a an author profile
+ *
+ * this author profile is data that is stored about a user for a book site
+ *
  */
 
+
+
+class Author  {
+	/**
+	 * id for this author; this is the primary key
+	 */
+	private $authorId;
+	/**
+	 * url for this authors avatar
+	 */
+	private $authorAvatarUrl;
+	/**
+	 * activation token for this author
+	 */
+	private $authorActivationToken;
+	/**
+	 * email address for this author
+	 */
+	private $authorEmail;
+	/**
+	 * hash for this author
+	 */
+	private $authorHash;
+	/**
+	 * username for this author
+	 */
+	private $authorUsername;
+
+	/**
+	 *accessor method for author id
+	 *
+	 * @return int value of profile id
+	 */
+	public function getAuthorId() {
+
+	}
+
+}
 ?>
