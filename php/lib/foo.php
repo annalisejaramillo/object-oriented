5014270011b93791c26155ddd6b68a19b81a6696
Annalise Jaramillo <annalise.jaramillo@gmail.com>
2019-07-18 22:09:01 -0600

    fixed errors

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -1,1 +1,9 @@
+<?php
+namespace Ajaramillo208\ObjectOriented;
 
+require_once("../classes/Foo.php");
+
+$pleaseWork = new Author("ad940772-8096-46bd-b10e-2ee205e1657f", "https://mir-s3-cdn-cf.behance.net/project_modules/disp/6278bb65663791.5afc1d2cb8cb8.jpg","aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "ajaramillo208@cnm.edu", '$argon2i$v=19$m=1024,t=384,p=2$dE55dm5kRm9DTEYxNFlFUA$nNEMItrDUtwnDhZ41nwVm7ncBLrJzjh5mGIjj8RlzVA',"annajaramillo",);
+
+
+var_dump($pleaseWork);
--- sql/author.sql
+++ sql/author.sql
@@ -1,5 +1,8 @@
+ALTER DATABASE ajaramillo208 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-create table author(
+DROP TABLE IF EXISTS Author;
+
+create table Author(
 	authorId binary(16) not null,
 	authorAvatarUrl varchar(255),
 	authorActivationToken char(32),
